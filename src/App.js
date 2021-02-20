a626f3a8f3523c7a41624bd245c0b46f5cdc1a0d
Shawn Truesdale <shawnctruesdale@gmail.com>
2021-02-20 12:01:14 -0700

    Add Login Landing page

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Dashboard from './modules/Dashboard/Dashboard';
 import DreamJournal from './modules/DreamJournal/DreamJournal';
 import NewDream from './modules/NewDream/NewDream';
 import Analytics from './modules/Analytics/Analytics';
+import Login from './modules/Login/Login';
 
 const App = () => {
   return (
     <main>
       <Switch>
-        <Route exact path="/" component={Dashboard} />
+        <Route exact path="/" component={Login} />
+        <Route exact path="/dashboard" component={Dashboard} />
         <Route exact path="/dreamjournal" component={DreamJournal} />
         <Route exact path="/newdream" component={NewDream} />
         <Route exact path="/analytics" component={Analytics} />
---
+++ src/modules/Login/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+  return (
+    <main>
+      <h1>Login</h1>
+      <Link to="/dashboard">LOGIN</Link>
+    </main>
+  );
+};
+
+export default Login;
