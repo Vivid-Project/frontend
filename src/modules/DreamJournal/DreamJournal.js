c9b20ab93f61d9641ae146f50d57dad18e8846bf
Shawn Truesdale <shawnctruesdale@gmail.com>
2021-02-20 11:57:15 -0700

    Add basic routing from homepage

---
+++ src/modules/DreamJournal/DreamJournal.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DreamJournal = () => {
+  return <h1>DreamJournal</h1>;
+};
+
+export default DreamJournal;
---
+++ src/modules/NewDream/NewDream.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NewDream = () => {
+  return <h1>NewDream</h1>;
+};
+
+export default NewDream;
