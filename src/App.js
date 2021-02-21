5fb5a7d28a9e9fdf6d43f174037e82fd148193bd
Shawn Truesdale <shawnctruesdale@gmail.com>
2021-02-20 18:39:04 -0700

    Create Form buildout with styling

--- src/App.js
+++ src/App.js
@@ -7,16 +7,20 @@ import NewDream from './modules/NewDream/NewDream';
 import Analytics from './modules/Analytics/Analytics';
 import Login from './modules/Login/Login';
 
+import 'fontsource-roboto';
+
 const App = () => {
   return (
-    <main>
-      <Switch>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/dreamjournal" component={DreamJournal} />
-        <Route exact path="/newdream" component={NewDream} />
-        <Route exact path="/analytics" component={Analytics} />
-      </Switch>
+    <main className="App">
+      <header className="App-header">
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/dreamjournal" component={DreamJournal} />
+          <Route exact path="/newdream" component={NewDream} />
+          <Route exact path="/analytics" component={Analytics} />
+        </Switch>
+      </header>
     </main>
   );
 };
--- src/modules/NewDream/NewDream.js
+++ src/modules/NewDream/NewDream.js
@@ -1,7 +1,62 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+import {
+  makeStyles,
+  ThemeProvider,
+  createMuiTheme,
+} from '@material-ui/core/styles';
+import { orange, blue } from '@material-ui/core/colors';
+
+const theme = createMuiTheme({
+  typography: {
+    h2: {
+      fontSize: 36,
+    },
+  },
+  palette: {
+    primary: {
+      main: orange[400],
+    },
+    secondary: {
+      main: blue[400],
+    },
+  },
+});
 
 const NewDream = () => {
-  return <h1>NewDream</h1>;
+  return (
+    <ThemeProvider theme={theme}>
+      <main>
+        <h1>NewDream</h1>
+        <form noValidate autoComplete="off">
+          <TextField
+            id="dream-title"
+            variant="standard"
+            color="primary"
+            label="Name Your Dream"
+            fullWidth
+          ></TextField>
+          <TextField
+            id="dream-body"
+            variant="outlined"
+            color="primary"
+            label="Describe Your Dream"
+            fullWidth
+            multiline
+            rowsMax={12}
+            style={{
+              margin: '15px 0',
+            }}
+          ></TextField>
+        </form>
+        <Button variant="contained" color="primary">
+          LOG
+        </Button>
+      </main>
+    </ThemeProvider>
+  );
 };
 
 export default NewDream;
