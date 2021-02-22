bfa1801d86b47eede0bc53db49727be8b3b02a03
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-02-22 05:48:47 -0700

    Add more fake data

--- src/data/fakeDreams.js
+++ src/data/fakeDreams.js
@@ -1,27 +1,34 @@
 const fakeDreams = {
   dreams: [
-    // {
-    //   id: 1,
-    //   date: 'January Wed 01',
-    //   title: 'Bear dream',
-    //   description: 'A bear bit me',
-    //   emotion: '1',
-    // },
-    // {
-    //   id: 2,
-    //   date: 'January Thur 02',
-    //   title: 'bug dream',
-    //   description: 'A bug landed on me',
-    //   emotion: '3',
-    // },
-    // {
-    //   id: 3,
-    //   date: 'January Fri 03',
-    //   title: 'Dog dream',
-    //   description: 'I was walking a dog',
-    //   emotion: '5',
-    // }
-  ]
+    {
+      id: 4,
+      date: 'January Wed 01',
+      title: 'Forest dream',
+      description: 'I was walking through a forest when I met a talking bird',
+      emotion: '1',
+    },
+    {
+      id: 1,
+      date: 'January Wed 01',
+      title: 'Bear dream',
+      description: 'A bear bit me',
+      emotion: '1',
+    },
+    {
+      id: 2,
+      date: 'January Thur 02',
+      title: 'Bug dream',
+      description: 'A bug landed on me',
+      emotion: '3',
+    },
+    {
+      id: 3,
+      date: 'January Fri 03',
+      title: 'Dog dream',
+      description: 'I was walking a dog',
+      emotion: '5',
+    },
+  ],
 }
 
 export default fakeDreams;
