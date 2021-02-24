1287cc71167c930d3a7beb41af04ee7d4064cb54
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-02-24 09:28:41 -0700

    Begin Dashbard unit test

---
+++ src/modules/Dashboard/Dashboard.test.js
@@ -0,0 +1,28 @@
+import { screen, render } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { BrowserRouter } from 'react-router-dom'
+import userEvent from '@testing-library/user-event'
+import Dashboard from './Dashboard'
+import fakeTone from '../../data/fakeTone'
+
+
+describe.only('Dashboard', () => {
+  it('should render the dashboard', () => {
+    render(
+      <BrowserRouter>
+        <Dashboard />
+      </BrowserRouter>
+    )
+    expect(screen.getByText('The Dashboard')).toBeInTheDocument()
+    expect(screen.getByTestId('addButton')).toBeInTheDocument()
+  })
+
+  it('should render a message if there are no dreams', () => {
+    render(
+      <BrowserRouter>
+        <Dashboard />
+      </BrowserRouter>
+    )
+    expect(screen.getByText('You have not saved any dreams yet')).toBeInTheDocument()
+  })
+})
