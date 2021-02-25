5e49b30927027e084d636e94c91436704cfdd4f3
Shawn Truesdale <shawnctruesdale@gmail.com>
2021-02-25 16:39:21 -0700

    Create fake User data

--- src/API/APIcalls.js
+++ src/API/APIcalls.js
@@ -1,19 +1,22 @@
+import user from '../data/fakeUser';
+
 export const fetchUserLogin = async (email) => {
-  try {
-    const response = await fetch(
-      'https://vivid-project-backend.herokuapp.com/users/authenticate',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email: email,
-        }),
-      }
-    );
-    return await response.json();
-  } catch (error) {
-    return console.log(error);
-  }
+  return user;
+  // try {
+  //   const response = await fetch(
+  //     'https://vivid-project-backend.herokuapp.com/users/authenticate',
+  //     {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify({
+  //         email: email,
+  //       }),
+  //     }
+  //   );
+  //   return await response.json();
+  // } catch (error) {
+  //   return console.log(error);
+  // }
 };
---
+++ src/data/fakeUser.js
@@ -0,0 +1,8 @@
+const user = {
+  id: 23,
+  name: "Patches O'Hoolihan",
+  email: 'patches@gmail.com',
+  token: 'erfgk45t6t459rgtngrtkj54no5v3n',
+};
+
+export default user;
