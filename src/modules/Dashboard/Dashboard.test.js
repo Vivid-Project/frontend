65124793aa71e51898b1f31a74930cf8edfbc6d5
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-02-26 10:39:07 -0700

    Fix testing

--- src/modules/Dashboard/Dashboard.test.js
+++ src/modules/Dashboard/Dashboard.test.js
@@ -1,29 +1,40 @@
-import { screen, render } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import { BrowserRouter } from 'react-router-dom';
-import userEvent from '@testing-library/user-event';
-import Dashboard from './Dashboard';
-import fakeTone from '../../data/fakeTone';
+import { screen, render } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { MemoryRouter } from 'react-router-dom'
+import userEvent from '@testing-library/user-event'
+import Dashboard from './Dashboard'
+import { fetchUserDreams } from '../../API/APIcalls'
+import user from '../../data/fakeUser'
+import UserContext from '../Context/UserContext'
+import fakeDreams from '../../data/fakeDreams'
+jest.mock('../../API/APIcalls')
 
 describe('Dashboard', () => {
   it('should render the dashboard', () => {
+    fetchUserDreams.mockResolvedValueOnce(fakeDreams.dreams)
     render(
-      <BrowserRouter>
-        <Dashboard />
-      </BrowserRouter>
-    );
-    expect(screen.getByText('The Dashboard')).toBeInTheDocument();
-    expect(screen.getByTestId('addButton')).toBeInTheDocument();
-  });
+      <MemoryRouter>
+        <UserContext.Provider value={user}>
+          <Dashboard />
+        </UserContext.Provider>
+      </MemoryRouter>
+    )
+    expect(screen.getByText('Welcome')).toBeInTheDocument()
+    expect(screen.getByTestId('addButton')).toBeInTheDocument()
+    screen.debug()
+  })
 
   it('should render a message if there are no dreams', () => {
+    fetchUserDreams.mockResolvedValueOnce()
     render(
-      <BrowserRouter>
-        <Dashboard />
-      </BrowserRouter>
-    );
+      <MemoryRouter>
+        <UserContext.Provider value={user}>
+          <Dashboard />
+        </UserContext.Provider>
+      </MemoryRouter>
+    )
     expect(
       screen.getByText('You have not saved any dreams yet')
-    ).toBeInTheDocument();
-  });
-});
+    ).toBeInTheDocument()
+  })
+})
