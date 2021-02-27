9ddb657c64d4d8c5f441ebec9d97372466d1c9a6
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-02-27 10:45:41 -0700

    Fix month display

--- src/modules/NewDream/NewDream.js
+++ src/modules/NewDream/NewDream.js
@@ -51,7 +51,7 @@ const NewDream = (props) => {
 
   const createDate = () => {
     let d = new Date();
-    return `${d.getFullYear()}/${d.getMonth()}/${d.getDate()}`;
+    return `${d.getFullYear()}/${d.getMonth()+ 1}/${d.getDate()}`;
   };
 
   useEffect(() => {
