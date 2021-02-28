7ef21d4811fd70624200738a2f187eb21c39f725
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-02-27 17:49:25 -0700

    Add pie chart to cards

--- src/modules/DreamCard/DreamCard.js
+++ src/modules/DreamCard/DreamCard.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import PieChart from '../../common/ToneGraph';
 import {
   Grid,
   Card,
@@ -7,42 +8,35 @@ import {
   CardContent,
   IconButton,
   Collapse,
-  Typography
+  Typography,
 } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { orange } from '@material-ui/core/colors';
 import { makeStyles, ThemeProvider } from '@material-ui/core/styles';
 import { theme } from '../../themes/theme';
+import fakeTone from '../../data/fakeTone';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     color: 'orange',
     background: '#282c34',
-  },
-  title: {
     display: 'flex',
-    justifyContent: 'center',
-    alignContent: 'center',
-    color: 'orange',
-  },
-  outterCard: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
+    // justifyContent: 'center',
+    flexDirection: 'column',
     alignItems: 'center',
-    outline: 'none',
-    color: 'orange',
-    background: '#282c34',
-    fontWeight: 400,
   },
   card: {
     display: 'flex-box',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
+    alignItems: 'stretch',
     color: 'orange',
     width: '17em',
     background: '#282c34',
     borderColor: 'orange',
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
   },
   expand: {
     transform: 'rotate(180deg)',
@@ -64,42 +58,61 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const DreamCard = ({ id, date, title, description, toneAnalysis }) => {
+  const [tones, setTones] = useState([]);
   const classes = useStyles();
   const [dreams, setDreams] = useState([]);
   const [expandedId, setExpandedId] = useState(-1);
+  const toneLabels = Object.keys(tones);
+  const toneValues = Object.values(tones);
+  useEffect(() => {
+    setTones(fakeTone.toneStrength);
+  });
 
   const handleExpandClick = (i) => {
     setExpandedId(expandedId === i ? -1 : i);
   };
 
+  // const toneLabels = Object.keys(toneAnalysis);
+  // const toneValues = Object.values(toneAnalysis);
+
   return (
     <ThemeProvider theme={theme}>
       <div>
-        <Grid className={classes.palette.primary}>
+        <Grid className={classes.palette.primary} container justify='center'>
           <Card
             key={id}
             id={id}
-            style={{ margin: '1em' }}
+            style={{ margin: '1em'}}
             className={classes.card}
           >
-            <CardHeader title={title} style={{ padding: '0' }} />
-            <Typography color="orange">
+            {' '}
+            <Typography
+              variant="subtitle1"
+              color="orange"
+              style={{ marginTop: '.5em', textAlign: 'left' }}
+            >
               {date}
             </Typography>
-            <CardActions disableSpacing>
+            <CardHeader title={title} style={{ padding: '0' }} />
+            <CardActions
+              disableSpacing
+              variant="subtitle2"
+              style={{ textAlign: 'right' }}
+            >
               <IconButton
                 className={classes.root}
                 onClick={() => handleExpandClick(id)}
                 aria-expanded={expandedId === id}
                 aria-label="show more"
+                style={{ textAlign: 'right' }}
               >
-                <ExpandMoreIcon />
+                <ExpandMoreIcon style={{ textAlign: 'right' }} />
               </IconButton>
             </CardActions>
             <Collapse in={expandedId === id} timeout="auto" unmountOnExit>
-              <CardContent>
+              <CardContent className={classes.content}>
                 <p>{description}</p>
-                {/* <p>Dream tones: {toneAnalysis.unique_tones}</p> */}
+                <PieChart toneLabels={toneLabels} toneValues={toneValues} />
               </CardContent>
             </Collapse>
           </Card>
