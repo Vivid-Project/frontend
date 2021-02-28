144d430795a9df498873680251c1536f74ff73cb
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-02-27 20:21:58 -0700

    Add bar graph

---
+++ src/common/ToneBar.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+
+const HorizontalTones = ({ toneValues, toneLabels}) => {
+  const data = {
+    labels: toneLabels,
+    datasets: [
+      {
+        label: 'Tone of dream',
+        data: toneValues,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+  
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  };
+  
+  return (
+    <>
+      <Bar data={data} options={options} />
+    </>
+  );
+}
+
+
+export default HorizontalTones;
