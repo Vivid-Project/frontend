724135e1af2df57309c19541ef07a1e31ff56b6f
Shawn Truesdale <shawnctruesdale@gmail.com>
2021-02-28 10:37:37 -0700

    Create datasets for all tones

--- src/common/LineGraph.js
+++ src/common/LineGraph.js
@@ -11,6 +11,7 @@ const TonesOverTime = () => {
   const [chartDayCount, setChartCount] = useState(7);
   const [chartDates, setChartDates] = useState(null);
   const [chartTones, setChartTones] = useState([]);
+  const [chartPlotDatasets, setChartPlotDatasets] = useState([]);
 
   useEffect(() => {
     buildChartDates();
@@ -23,12 +24,8 @@ const TonesOverTime = () => {
     if (!chartDates) return;
     processDreamData();
     console.log(chartTones);
-    // plotChartTones();
+    createPlotChartDatasets();
   }, [chartDates]);
-
-  // const plotChartTones = () => {
-
-  // }
 
   const getDateToday = (dayModifier) => {
     const date = new Date();
@@ -59,7 +56,7 @@ const TonesOverTime = () => {
       if (!toneData[date]) {
         toneData[date] = 0;
       }
-      dateValues.push(toneData[date]);
+      dateValues.unshift(toneData[date]);
       return dateValues;
     }, []);
     setChartTones(chartTones.push({ [tone]: toneDates }));
@@ -93,11 +90,11 @@ const TonesOverTime = () => {
     buildToneValues(toneDatesAndFreqs);
   };
 
-  const data = {
-    labels: chartDates,
-    datasets: [
-      {
-        label: 'My First dataset',
+  const createPlotChartDatasets = () => {
+    const chartPlotData = chartTones.map((tone) => {
+      // debugger;
+      return {
+        label: Object.keys(tone),
         fill: false,
         lineTension: 0.1,
         backgroundColor: 'rgba(75,192,192,0.4)',
@@ -115,9 +112,15 @@ const TonesOverTime = () => {
         pointHoverBorderWidth: 2,
         pointRadius: 1,
         pointHitRadius: 10,
-        data: [65, 59, 80, 81, 56, 55, 40],
-      },
-    ],
+        data: Object.values(tone)[0],
+      };
+    });
+    setChartPlotDatasets(chartPlotData);
+  };
+
+  const data = {
+    labels: chartDates,
+    datasets: chartPlotDatasets,
   };
 
   return <Line data={data} />;
