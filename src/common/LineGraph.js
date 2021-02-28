a7e963127a2aa030fd3744d56fa1af29335bea4c
Shawn Truesdale <shawnctruesdale@gmail.com>
2021-02-28 11:06:38 -0700

    Randomize color for each line on chart

--- src/common/LineGraph.js
+++ src/common/LineGraph.js
@@ -90,19 +90,27 @@ const TonesOverTime = () => {
     buildToneValues(toneDatesAndFreqs);
   };
 
+  const getRandomColor = () => {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+
   const createPlotChartDatasets = () => {
     const chartPlotData = chartTones.map((tone) => {
-      // debugger;
       return {
         label: Object.keys(tone),
         fill: false,
-        lineTension: 0.1,
+        lineTension: 0.2,
         backgroundColor: 'rgba(75,192,192,0.4)',
-        borderColor: 'rgba(75,192,192,1)',
+        borderColor: getRandomColor(),
         borderCapStyle: 'butt',
         borderDash: [],
         borderDashOffset: 0.0,
-        borderJoinStyle: 'miter',
+        borderJoinStyle: 'round',
         pointBorderColor: 'rgba(75,192,192,1)',
         pointBackgroundColor: '#fff',
         pointBorderWidth: 1,
@@ -110,7 +118,7 @@ const TonesOverTime = () => {
         pointHoverBackgroundColor: 'rgba(75,192,192,1)',
         pointHoverBorderColor: 'rgba(220,220,220,1)',
         pointHoverBorderWidth: 2,
-        pointRadius: 1,
+        pointRadius: 0,
         pointHitRadius: 10,
         data: Object.values(tone)[0],
       };
