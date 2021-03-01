905283a4eaff470c04526b6b9d4824451f63c03f
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-03-01 10:22:57 -0700

    Add chart to dashboard

--- src/modules/Dashboard/Dashboard.js
+++ src/modules/Dashboard/Dashboard.js
@@ -1,17 +1,14 @@
-import React, { useState, useEffect, useContext } from 'react';
-// import { Link } from 'react-router-dom';
-import PieChart from '../../common/ToneGraph';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import UserContext from '../Context/UserContext';
-// import DreamJournal from '../DreamJournal/DreamJournal';
 import TonesOverTime from '../../common/TonesOverTime'
 
 import { theme } from '../../themes/theme';
-// import { Container, Grid, AppBar, Fab } from '@material-ui/core';
-// import Toolbar from '@material-ui/core/Toolbar';
+import { AppBar, Fab } from '@material-ui/core';
+import Toolbar from '@material-ui/core/Toolbar';
 import { makeStyles, ThemeProvider } from '@material-ui/core/styles';
-// import AddIcon from '@material-ui/icons/Add';
+import AddIcon from '@material-ui/icons/Add';
 
-import fakeTone from '../../data/fakeTone';
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
@@ -30,17 +27,13 @@ const useStyles = makeStyles((theme) => ({
     right: 0,
     margin: '0 auto',
   },
+  graph: {
+    marginBottom: '2em'
+  }
 }));
 
 const Dashboard = () => {
-  // const [tones, setTones] = useState([]);
   const user = useContext(UserContext);
-  // const toneLabels = Object.keys(tones);
-  // const toneValues = Object.values(tones);
-  // useEffect(() => {
-  //   setTones(fakeTone.toneStrength);
-  // });
-
   const classes = useStyles();
 
   return (
@@ -48,21 +41,9 @@ const Dashboard = () => {
       <main>
         {/* <Container> */}
         <h3>Welcome{user.name && <span>, {user.name.split(' ')[0]}</span>}</h3>
-        <>
-          <h5>My Dream Data</h5>
+        <div className={classes.graph}>
           <TonesOverTime />
-        </>
-
-        {/* <Grid>
-            {!toneValues.length && (
-              <h6>You do not have any data about dream tones yet</h6>
-            )}
-            <PieChart toneLabels={toneLabels} toneValues={toneValues} />
-          </Grid>
-          <Grid>
-            <DreamJournal />
-          </Grid>
-        </Container>
+        </div>
         <AppBar position="fixed" className={classes.appBar}>
           <Toolbar variant="dense">
             <Link to="/newdream">
@@ -75,7 +56,7 @@ const Dashboard = () => {
               </Fab>
             </Link>
           </Toolbar>
-        </AppBar> */}
+        </AppBar>
       </main>
     </ThemeProvider>
   );
