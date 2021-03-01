3d6ee8ec6b96013ea38b690537a748793006ba43
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-03-01 10:20:51 -0700

    Remove Data section

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import './App.css';
 import Dashboard from './modules/Dashboard/Dashboard';
 import DreamJournal from './modules/DreamJournal/DreamJournal';
 import NewDream from './modules/NewDream/NewDream';
-import Analytics from './modules/Analytics/Analytics';
 import Login from './modules/Login/Login';
 import Header from './modules/Header/Header';
 
@@ -30,7 +29,6 @@ const App = () => {
               <Route exact path="/dashboard" component={Dashboard} />
               <Route exact path="/dreamjournal" component={DreamJournal} />
               <Route exact path="/newdream" component={NewDream} />
-              <Route exact path="/analytics" component={Analytics} />
             </>
           </Switch>
         </header>
