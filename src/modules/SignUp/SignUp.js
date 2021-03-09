d281c6c2f23e9e85ada25ba98d115f878d400793
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-03-09 16:39:22 -0700

    Add SignUp componet for new users

---
+++ src/modules/SignUp/SignUp.js
@@ -0,0 +1,138 @@
+import React, { useState, useContext, useEffect } from 'react';
+import { withRouter } from 'react-router-dom';
+import * as API from '../../API/APIcalls';
+import { makeStyles, ThemeProvider, useTheme } from '@material-ui/core/styles';
+import image from '../../assets/login-landing-back.jpg';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+
+import {
+  FilledInput,
+  Button,
+  InputAdornment,
+  IconButton,
+  Container,
+} from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    background: `url(${image})`,
+    backgroundSize: 'cover',
+    width: '100%',
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  input: {
+    margin: theme.spacing(1.5),
+    width: '30ch',
+  },
+}));
+
+const SignUp = (props) => {
+  const theme = useTheme();
+  const classes = useStyles();
+  const [signUpError, setSignUpError] = useState(false);
+  const [disabled, setDisabled] = useState(false);
+  const [values, setValues] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  useEffect(() => {
+    if (!values.name || !values.email || !values.password) {
+      setDisabled(true);
+      return;
+    }
+    setDisabled(false);
+  }, [values.name, values.email, values.password]);
+
+  const createUser = (props) => {
+    let { history, setUser } = props;
+    API.createNewUser(values.name, values.email, values.password)
+      .then((response) => {
+        setUser({
+          id: response.id,
+          name: response.name,
+          email: response.email,
+          token: `Bearer ${response.token}`,
+        });
+      })
+      .then(() => history.push('/dashboard'))
+      .catch((error) => setSignUpError(true));
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <main className={classes.root}>
+        <form noValidate autoComplete='off' className={classes.root}>
+          <h1 style={{ marginTop: theme.spacing(15) }}>VIVID</h1>
+          <FilledInput
+            id='name'
+            color='primary'
+            placeholder='Name'
+            className={classes.input}
+            onChange={handleChange('name')}
+          ></FilledInput>
+          <FilledInput
+            id='email'
+            color='primary'
+            placeholder='Email'
+            className={classes.input}
+            onChange={handleChange('email')}
+          ></FilledInput>
+          <FilledInput
+            id='password'
+            color='primary'
+            placeholder='Password'
+            type={values.showPassword ? 'text' : 'password'}
+            className={classes.input}
+            onChange={handleChange('password')}
+            endAdornment={
+              <InputAdornment position='end'>
+                <IconButton
+                  aria-label='toggle password visibility'
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                >
+                  {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            }
+          ></FilledInput>{' '}
+          {signUpError && <h5>Oh no there was an error please try again</h5>}
+          <Button
+            variant='contained'
+            color='primary'
+            onClick={createUser}
+            disabled={disabled}
+            style={{ margin: '1em' }}
+          >
+            Sign Up!
+          </Button>
+            Already have an account?
+            <Button variant='contained' color='primary' href='/' style={{marginTop: '1em'}}>
+              Login
+            </Button>
+        </form>
+      </main>
+    </ThemeProvider>
+  );
+};
+
+export default withRouter(SignUp);
