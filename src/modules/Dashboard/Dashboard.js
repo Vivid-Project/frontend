56fab2a2074754563e13c05107df3a1545cb5bf5
Shawn Truesdale <shawnctruesdale@gmail.com>
2021-03-15 11:20:55 -0600

    Add alternate styling to Appbar on desktop view

--- src/modules/Dashboard/Dashboard.js
+++ src/modules/Dashboard/Dashboard.js
@@ -4,7 +4,13 @@ import UserContext from '../Context/UserContext';
 import TonesOverTime from '../../Analytics/TonesOverTime';
 
 import { theme } from '../../themes/theme';
-import { AppBar, Fab, Toolbar, Container } from '@material-ui/core';
+import {
+  AppBar,
+  Fab,
+  Toolbar,
+  Container,
+  useMediaQuery,
+} from '@material-ui/core';
 import { makeStyles, ThemeProvider } from '@material-ui/core/styles';
 import AddIcon from '@material-ui/icons/Add';
 
@@ -25,38 +31,60 @@ const useStyles = makeStyles((theme) => ({
     right: 0,
     margin: '0 auto',
   },
-  graph: {
-    marginBottom: '2em',
-    '& .MuiSvgIcon-root': {
-      color: 'orange',
-    },
+  desktopFabButton: {
+    background: '#ff5722',
+    position: 'fixed',
+    zIndex: 1,
+    bottom: 30,
+    right: 30,
+    // margin: '0 auto',
   },
 }));
 
 const Dashboard = () => {
   const user = useContext(UserContext);
   const classes = useStyles();
+  const isMobile = useMediaQuery(theme.breakpoints.down('xs'));
 
   return (
     <ThemeProvider theme={theme}>
       <main>
         <h3>Welcome{user.name && <span>, {user.name.split(' ')[0]}</span>}</h3>
-        <Container className={classes.graph} component="div" maxWidth="sm">
+        <Container component="div" maxWidth="sm">
           <TonesOverTime />
         </Container>
-        <AppBar position="fixed" className={classes.appBar}>
-          <Toolbar variant="dense">
-            <Link to="/newdream">
-              <Fab
-                aria-label="add"
-                data-testid="addButton"
-                className={classes.fabButton}
-              >
-                <AddIcon />
-              </Fab>
-            </Link>
-          </Toolbar>
-        </AppBar>
+        {isMobile ? (
+          <>
+            <AppBar position="fixed" className={classes.appBar}>
+              <Toolbar variant="dense">
+                <Link to="/newdream">
+                  <Fab
+                    aria-label="add"
+                    data-testid="addButton"
+                    className={classes.fabButton}
+                  >
+                    <AddIcon />
+                  </Fab>
+                </Link>
+              </Toolbar>
+            </AppBar>
+          </>
+        ) : (
+          <>
+            {' '}
+            <AppBar position="fixed" className={classes.appBar}>
+              <Link to="/newdream">
+                <Fab
+                  aria-label="add"
+                  data-testid="addButton"
+                  className={classes.desktopFabButton}
+                >
+                  <AddIcon />
+                </Fab>
+              </Link>
+            </AppBar>
+          </>
+        )}
       </main>
     </ThemeProvider>
   );
